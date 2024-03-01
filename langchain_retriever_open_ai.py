48119da5af419c4da84b2f3c6a1925b3ac1926e8
EduCasM <53205307+EduCasM@users.noreply.github.com>
2024-02-29 21:01:52 -0600

    Updating the  langchain_retriever_open_ai.py file
    making some changes to adapt this file to our specific Loka use case

--- langchain_retriever_open_ai.py
+++ langchain_retriever_open_ai.py
@@ -14,9 +14,10 @@ def build_chain():
   retriever = AmazonKendraRetriever(index_id=kendra_index_id,region_name=region)
 
   prompt_template = """
-  The following is a friendly conversation between a human and an AI. 
-  The AI is talkative and provides lots of specific details from its context.
-  If the AI does not know the answer to a question, it truthfully says it 
+  The following is a friendly conversation between a human and an AI assistant. 
+  The Loka assistant is talkative and provides lots of specific details from its context 
+  (AWS SageMaker documentation).
+  If the assistant does not know the answer to a question, it truthfully says it 
   does not know.
   {context}
   Instruction: Based on the above documents, provide a detailed answer for, {question} Answer "don't know" 
@@ -44,9 +45,14 @@ def run_chain(chain, prompt: str, history=[]):
     }
 
 if __name__ == "__main__":
-    chain = build_chain()
-    result = run_chain(chain, "What's SageMaker?")
-    print(result['answer'])
+    questions = ["What's SageMaker?", "What are all AWS regions where SageMaker is available?", "How to check if an endpoint is KMS encrypted?",
+                "What are SageMaker Geospatial capabilities?"]
+    
+    for question in questions:
+        chain = build_chain()
+        result = run_chain(chain, question)
+        print(result['answer'])
+      
     if 'source_documents' in result:
         print('Sources:')
         for d in result['source_documents']:
